8f881b7d298a979545da8d3c30f26006ff3d94fb
Benoît de CHATEAUVIEUX <benoit.dechateauvieux@gmail.com>
2014-10-29 15:22:19 +0000

    MAJ des VM

--- vagrant-tomcat/jmxtrans/setenv.sh
+++ vagrant-tomcat/jmxtrans/setenv.sh
@@ -1,3 +1,5 @@
 export JAVA_OPTS="$JAVA_OPTS -javaagent:/vagrant/jmxtrans/jmxtrans-agent-1.0.6.jar=/vagrant/jmxtrans/jmxtrans-agent.xml"
 export JAVA_OPTS="$JAVA_OPTS -XX:+CMSClassUnloadingEnabled -XX:+CMSPermGenSweepingEnabled"
 export JAVA_OPTS="$JAVA_OPTS -XX:PermSize=128m -XX:MaxPermSize=256m -Xmx1024M"
+
+export JAVA_OPTS="$JAVA_OPTS -javaagent:/vagrant/appd/javaagent.jar" 
--- vagrant-tomcat/tomcat-installer.sh
+++ vagrant-tomcat/tomcat-installer.sh
@@ -1,4 +1,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
+
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java7_64.sh && bash equip_java7_64.sh
+
 sudo apt-get install -y tomcat7 tomcat7-admin
 sudo cp /vagrant/jmxtrans/setenv.sh /usr/share/tomcat7/bin/setenv.sh
 sudo chmod 777 /usr/share/tomcat7/bin/setenv.sh
